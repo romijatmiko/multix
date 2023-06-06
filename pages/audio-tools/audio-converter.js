e74c90af5e1e5078ad33a866f0a26961d4e9e329
romijatmiko <romijatmiko033@gmail.com>
2023-06-06 22:01:23 +0700

    mulai pusing

--- pages/audio-tools/audio-converter.js
+++ pages/audio-tools/audio-converter.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { saveAs } from "file-saver";
 import lamejs from "lamejs";
 
 export default function AudioConverter() {
@@ -36,20 +37,36 @@ export default function AudioConverter() {
 					audioContext.sampleRate,
 					bitrate
 				);
-
 				const mp3Data = [];
 				let remaining = audioArray.length;
 
 				while (remaining >= 0) {
-					const samples = mp3Encoder.encodeBuffer(audioArray);
-					mp3Data.push(new Int8Array(samples));
-					remaining -= samples.length;
+					const left = audioArray.subarray(
+						audioArray.length - remaining,
+						audioArray.length - remaining + 1152
+					);
+					const mp3buf = mp3Encoder.encodeBuffer(left);
+
+					mp3Data.push(mp3buf);
+					remaining -= 1152;
 				}
 
-				const finalSamples = mp3Encoder.flush();
-				mp3Data.push(new Int8Array(finalSamples));
+				const mp3buf = mp3Encoder.flush();
 
-				const mp3Blob = new Blob(mp3Data, { type: "audio/mp3" });
+				if (mp3buf.length > 0) {
+					mp3Data.push(mp3buf);
+				}
+
+				const mergedMp3Data = new Uint8Array(
+					mp3Data.length * mp3Data[0].length
+				);
+
+				for (let i = 0, offset = 0; i < mp3Data.length; i++) {
+					mergedMp3Data.set(mp3Data[i], offset);
+					offset += mp3Data[i].length;
+				}
+
+				const mp3Blob = new Blob([mergedMp3Data], { type: "audio/mp3" });
 				const convertedFile = new File([mp3Blob], "converted.mp3", {
 					type: "audio/mp3",
 				});
@@ -60,6 +77,12 @@ export default function AudioConverter() {
 			reader.readAsArrayBuffer(inputFile);
 		} catch (error) {
 			console.error("Error converting audio:", error);
+		}
+	};
+
+	const handleDownload = () => {
+		if (convertedFile) {
+			saveAs(convertedFile);
 		}
 	};
 
@@ -75,13 +98,9 @@ export default function AudioConverter() {
 				onChange={handleBitrateChange}
 			/>
 			<button onClick={handleConvert}>Convert to MP3</button>
-			{convertedFile && (
-				<a
-					href={URL.createObjectURL(convertedFile)}
-					download={convertedFile.name}>
-					Download Converted File
-				</a>
-			)}
+			<button onClick={handleDownload} disabled={!convertedFile}>
+				Download Converted File
+			</button>
 		</div>
 	);
 }
